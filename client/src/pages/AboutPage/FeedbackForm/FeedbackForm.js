6a2f511f041df9d11ed4014fa9d976b322b2e737
HappyLoL <happylolonly@gmail.com>
2017-11-07 13:21:39 +0300

    use script to change prop-types library

--- client/src/pages/AboutPage/FeedbackForm/FeedbackForm.js
+++ client/src/pages/AboutPage/FeedbackForm/FeedbackForm.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import axios from 'axios';
 import toastr from 'toastr';
