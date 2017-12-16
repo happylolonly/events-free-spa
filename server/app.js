dc5c5b630c2cef4d66d7a32860956854155d161a
HappyLoL <happylolonly@gmail.com>
2017-12-16 19:30:18 +0300

    pass socket instance to parse function

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ const io = require('socket.io').listen(server);
 
 require('./helpers/db').default(mongoose, () => {
 
-  parse();
+  // parse(io);
 
   cron.schedule('* * 1 * * *', () => {
     console.log('running a task every hour');
