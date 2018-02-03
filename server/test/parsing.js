e38bf754360a2fac0776972da998d465a9b6a220
HappyLoL <happylolonly@gmail.com>
2018-02-03 14:42:30 +0300

    add some tests

--- server/test/parsing.js
+++ server/test/parsing.js
@@ -1,1 +1,14 @@
 const assert = require('assert');
+
+
+describe('first test', () => {
+
+	it('first test is passed', () => {
+		assert(true === true);
+	});
+
+	it('another test for travic', () => {
+		assert(2 === 2);
+	});
+
+});
