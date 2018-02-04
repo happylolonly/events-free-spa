9a1f771a1009bb79df9a2e58617ad0a267457ac5
HappyLoL <happylolonly@gmail.com>
2018-02-04 14:33:17 +0300

    change client tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
 
+import { BrowserRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import Routes from './routes';
 import App from './App';
 
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  const component = shallow(<App route={{ routes: [] }} />);
+describe('initial render', () => {
 
-  ReactDOM.render(<component />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    const Component = (
+      <BrowserRouter>
+        <div>{renderRoutes(Routes)}</div>
+      </BrowserRouter>
+    );
+    ReactDOM.render(<Component />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+
+  it('app renders with enzyme', () => {
+    const wrapper = shallow(<App route={{ routes: [] }} />);
+    expect(wrapper.find('.app')).toHaveLength(1);
+  });
+
 });
