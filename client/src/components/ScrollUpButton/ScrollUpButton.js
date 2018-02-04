408f9ced11dc4b42fb37b83a482d64015731552a
HappyLoL <happylolonly@gmail.com>
2018-02-04 14:38:10 +0300

    begin scroll up button

---
+++ client/src/components/ScrollUpButton/ScrollUpButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './ScrollUpButton.css';
+
+
+const propTypes = {
+
+};
+
+const ScrollUpButton = () => {
+  
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <div className="scroll-up-button" onClick={handleClick}>
+      dfl;dfl;d
+    </div>
+  );
+};
+
+ScrollUpButton.propTypes = propTypes;
+
+export default ScrollUpButton;
