97ac85ff8896915dc60d7c73d16c5fcc84437e8b
HappyLoL <happylolonly@gmail.com>
2018-02-25 23:39:23 +0300

    add Map component

--- client/src/pages/EventPage/Location/Location.js
+++ client/src/pages/EventPage/Location/Location.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Map from './Map';
 
 import './Location.scss';
+
+
 
 
 const propTypes = {
 //locaton
 }
 
-const Location = ({ location }) => {
-  return (
-    <div className="location">
+class Location extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isShowMap: false,
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isShowMap: !this.state.isShowMap });
+  }
+
+  render() {
+    return (
+
+      <div className="location">
       <h5>Место</h5>
-      <p>{location}</p>
-      <a href="#">Показать на карте</a>
+      <p>{this.props.location}</p>
+      <button  className="btn-link" onClick={this.handleClick}>{!this.state.isShowMap ? 'Показать на карте' : 'Скрыть'} </button>
+
+      {this.state.isShowMap &&       <Map location={this.props.location} />
+ }
+
     </div>
-  )
+
+    )
+  }
 }
 
 Location.propTypes = propTypes;
---
+++ client/src/pages/EventPage/Location/Map.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+const key = 'AIzaSyC6b_38Ikznrhtf5ZB8sBXm3Yl7y7zL8j8';
+
+
+
+
+
+class MyMapComponent2 extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+
+    componentDidMount() {
+        this.update();
+
+        
+
+    }
+    componentWillReceiveProps() {
+        this.update();
+    }
+
+    update() {
+        let geocoder = new window.google.maps.Geocoder();
+
+
+    geocoder.geocode( { 'address': this.props.location }, (results, status) => {
+        if (status == 'OK') {
+            console.log(results);
+
+
+let lat = results[0].geometry.location.lat()
+let lng = results[0].geometry.location.lng()
+
+this.setState({lat, lng})
+        } else {
+            console.log('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+
+
+    }
+
+    render() {
+        const { lat, lng } = this.state;
+        if (!lat || !lng) return null;
+
+        return (
+            <GoogleMap
+              defaultZoom={11}
+              defaultCenter={{ lat: lat, lng: lng }}
+            >
+              {this.props.isMarkerShown && <Marker position={{ lat: lat, lng: lng }} />}
+            </GoogleMap>
+          )
+    }
+}
+
+
+const MyMapComponent = withScriptjs(withGoogleMap(MyMapComponent2)
+)
+
+export default ({location}) => {
+    return <MyMapComponent
+    isMarkerShown
+    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC6b_38Ikznrhtf5ZB8sBXm3Yl7y7zL8j8&v=3.exp&libraries=geometry,drawing,places"
+    loadingElement={<div style={{ height: `100%` }} />}
+    containerElement={<div style={{ height: `400px` }} />}
+    mapElement={<div style={{ height: `100%` }} />}
+    location={location}
+  />
+}
+
+
