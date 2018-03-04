5eab98f2cc313743f945aa0a84233831d5e0af60
HappyLoL <happylolonly@gmail.com>
2018-03-04 22:51:30 +0300

    add test route for ssl

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,14 @@
 export default (app) => {
 
-  app.get('/', (req, res) => {
+  app.get('/', (req, res, next) => {
+    // if (req.originalUrl.slice(-1) != '/') return next();
     res.sendFile(__dirname + '/../static/build/index.html');
+  });
+
+  app.get('/.well-known/acme-challenge/*', (req, res, next) => {
+    // if (req.originalUrl.slice(-1) != '/') return next();
+    res.send(200);
+    // res.sendFile(__dirname + '/../static/build/index.html');
   });
 
   require('./events')(app);
