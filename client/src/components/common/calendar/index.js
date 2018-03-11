95943d3b928b18a2855296e5491c14bcbc0c0f7e
HappyLoL <happylolonly@gmail.com>
2018-03-11 16:55:59 +0300

    add calendar component

---
+++ client/src/components/common/calendar/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactCalendar from 'react-calendar';
+
+import './index.scss';
+
+const propTypes = {
+    // onChange
+    // name
+    // value
+
+};
+
+
+const Calendar = ({ onChange, value, name }) => {
+
+    function handleChange(date) {
+        onChange(date, name);
+    }
+
+    return (
+        <div>
+        <ReactCalendar
+        onChange={handleChange}
+        value={value}
+        />
+  </div>
+    );
+  };
+
+Calendar.propTypes = propTypes;
+
+export default Calendar;
--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -1,2 +1,7 @@
+import Calendar from './calendar';
 export * from './Loader';
 export * from './Checkbox';
+// export * from './calendar';
+
+
+export { Calendar };
