0040c508f5db4e251f2d4e05445e5422d4860dd4
HappyLoL <happylolonly@gmail.com>
2018-03-26 22:44:00 +0300

    add parse config

--- server/helpers/parse.js
+++ server/helpers/parse.js
@@ -1,41 +1,41 @@
 import meetupBy from '../parse/meetupBy';
 import eventsDevBy from '../parse/eventsDevBy';
 import imaguru from '../parse/imaguru';
-import vk from '../parse/vk';
 import sportMts from '../parse/sportMts';
-
 import tutby from '../parse/tutby';
-
 import space from '../parse/space';
 import htp from '../parse/htp';
 import daonlp from '../parse/daonlp';
-
 import citydogVedy from '../parse/citydogVedy';
 import citydogAfisha from '../parse/citydogAfisha';
+import vk from '../parse/vk';
+
+import config from './parse-config';
 
 
 export default (io) => {
-
-  meetupBy.init();
-  eventsDevBy.init();
-  imaguru.init();
-  sportMts.init();
-
-  tutby.init();
-
-  // htp.init();
-  // space.init();
-  // daonlp.init();
-
-  citydogAfisha.init();
-
-  setTimeout(() => { // citydog blocks
-    citydogVedy.init();
-  }, 1000);
+  config.meetupBy && meetupBy.init();
+  config.eventsDevBy && eventsDevBy.init();
+  config.imaguru && imaguru.init();
   
-  vk.init('minskforfree');
-  vk.init('free_fitness_minsk');
-  vk.init('free_languages_minsk');
+  config.sportMts && sportMts.init();
+  config.tutby && tutby.init();
+
+  config.htp && htp.init();
+  config.space && space.init();
+  config.daonlp && daonlp.init();
+
+  config.citydogAfisha && citydogAfisha.init();
+
+  if (config.citydogVedy) {
+    setTimeout(() => { // citydog blocks
+      citydogVedy.init();
+    }, 1000);
+  }
+  
+  config.minskforfree && vk.init('minskforfree');
+  config.freeFitnessMinsk && vk.init('free_fitness_minsk');
+  config.freeLanguagesMinsk && vk.init('free_languages_minsk');
 
   setTimeout(() => {
     io.sockets.emit('events-updated');
