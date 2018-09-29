a214a592fe5aeb012ee8f50a09aa5401f88d6958
HappyLoL <happylolonly@gmail.com>
2018-09-29 21:04:02 +0300

    change getting config logic

--- server/configs/index.js
+++ server/configs/index.js
@@ -1,14 +1,9 @@
-// не лазьте тут плз)
+let config;
 
-export default {
-  port: 3090,
-  vk: {
-    app: 6131483,
-    key: 'f4vLOjoPyKSOw4qXgb6y',
-    token: 'b58844e3b58844e3b58844e34eb5d5cbf8bb588b58844e3ecf6456263d1070e24bb2a38',
-  },
-  db: {
-    url: 'mongodb://HappyLoL:12345678@ds061246.mlab.com:61246/cubes',
-  },
-  redis: 'redis-15117.c12.us-east-1-4.ec2.cloud.redislabs.com'
-};
+if (process.env.NODE_ENV === 'production') {
+  config = require('./production');
+} else {
+  config = require('./development');
+}
+
+export default config;
---
+++ server/configs/production.js
@@ -0,0 +1,12 @@
+export default {
+  "port": 3090,
+  "vk": {
+    "app": 6131483,
+    "key": "f4vLOjoPyKSOw4qXgb6y",
+    "token": "b58844e3b58844e3b58844e34eb5d5cbf8bb588b58844e3ecf6456263d1070e24bb2a38"
+  },
+  "db": {
+    "url": process.env.MONGO_URI
+  },
+  "redis": "redis-15117.c12.us-east-1-4.ec2.cloud.redislabs.com"
+}
