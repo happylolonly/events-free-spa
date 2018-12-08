db1cc860aac1217eacc9e52aab624ccdec331f77
Cheslav Zhuravskiy <happylolonly@gmail.com>
2018-12-09 02:40:18 +0300

    some refactor

--- server/routes/ssr.js
+++ server/routes/ssr.js
@@ -2,18 +2,10 @@ import puppeteer from 'puppeteer';
 import EventModel from '../model/event';
 import moment from 'moment';
 
-
-// In-memory cache of rendered pages. Note: this will be cleared whenever the
-// server process stops. If you need true persistence, use something like
-// Google Cloud Storage (https://firebase.google.com/docs/storage/web/start).
 const RENDER_CACHE = new Map();
 
 export async function ssr(url, path) {
-
   console.log(url, path);
-
-  const fullUrl = url.replace('/index.html', path);
-  console.log(fullUrl);
 
   if (RENDER_CACHE.has(path)) {
     return {html: RENDER_CACHE.get(path), ttRenderMs: 0};
@@ -23,53 +15,27 @@ export async function ssr(url, path) {
 
   const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
+
   try {
-    // networkidle0 waits for the network to be idle (no requests for 500ms).
-    // The page's JS has likely produced markup by this point, but wait longer
-    // if your site lazy loads, etc.
     await page.goto(url, {waitUntil: 'networkidle0'});
-    // await page.goto(path, {waitUntil: 'networkidle0'});
-    await page.waitForSelector('.app'); // ensure #posts exists in the DOM.
+    await page.waitForSelector('.app');
 
     await page.evaluate((path)=>{
-        // const a = document.querySelector('a');
-        // a.href = path;
-        // a.id = 'puppetuerLink';
-        // a.innerHTML = 'dskslddsklsdk';
-        // // const g = document.querySelector('.app');
-        // // g.prepend(a);
-        // // return el.contentWindow.test;
-        window.browserHistory.push(path);
+      window.browserHistory.push(path);
+    }, path)
 
-      }, path)
-
-    // await page.$(`puppetuerLink`);
-
-
-
-
-    // let selector;
-    // if (path.includes('/about') || path.includes('/settings')) {
-    //     selector = path;
-    // } else if ()
-    // if (path !== '/events') {
-        // const ele = await page.$('#puppetuerLink')
-        // await ele.click({delay: 800});
-        await page.waitFor(2000);
-        // await page.waitFr(3000);
-    // }
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
     throw new Error('page.goto/waitForSelector timed out.');
   }
 
-  const html = await page.content(); // serialized HTML of page DOM.
+  const html = await page.content();
   await browser.close();
 
   const ttRenderMs = Date.now() - start;
   console.info(`Headless rendered page in: ${ttRenderMs}ms`);
 
-  RENDER_CACHE.set(path, html); // cache rendered page.
+  RENDER_CACHE.set(path, html);
 
   return {html, ttRenderMs};
 }
@@ -115,6 +81,5 @@ export async function preload() {
   } catch (error) {
     console.log(error);
   }
-  debugger;
   console.log(RENDER_CACHE);
 }
