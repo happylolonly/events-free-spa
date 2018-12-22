0e049e560a6aded43900becbe7dc8233ffc44c73
Cheslav Zhuravskiy <happylolonly@gmail.com>
2018-12-22 15:47:11 +0300

    remove footer rights

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -24,7 +24,7 @@ const Footer = () => {
         </ul>
       </div>
 
-      <p>© 2017 - {new Date().getFullYear()} Events Free. All rights reserved. Making with love {isChristmasHolidays() ? <ChristmasTree /> : <span>❤</span>}</p>
+      <p>© 2017 - {new Date().getFullYear()} Events Free. Making with love {isChristmasHolidays() ? <ChristmasTree /> : <span>❤</span>}</p>
     </footer>
   )
 }
