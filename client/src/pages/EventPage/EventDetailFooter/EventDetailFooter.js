adae309ed6557f0a59558f5b9281fd4ed660da5a
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-01-06 00:48:57 +0300

    format file

--- client/src/pages/EventPage/EventDetailFooter/EventDetailFooter.js
+++ client/src/pages/EventPage/EventDetailFooter/EventDetailFooter.js
@@ -24,40 +24,40 @@ class EventDetailFooter extends Component {
 
     this.state = {
       isShowMap: false,
-    }
+    };
 
-    this.handleClick = this.handleClick.bind(this); 
-
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
     this.setState({ isShowMap: !this.state.isShowMap });
   }
 
-
-
   render() {
-      const { title, location, id, contacts, image } = this.props;
+    const { title, location, id, contacts, image } = this.props;
     return (
-
-        <div className="additional-info">
-            <div className="first-row">
-                <SocialButtons
-                link={`https://www.eventsfree.by/event/${id}`}
-                title={title}
-                isShowCount={false}
-                image={image}/>
-              {contacts && Object.keys(contacts).length > 0 && <Contacts contacts={contacts} />}
-              {location && 
-              <div>
-                <Location location={location}/>
-                <button  className="btn-link" onClick={this.handleClick}>{!this.state.isShowMap ? 'Показать на карте' : 'Скрыть'} </button>
-              </div>}
-              <div>
-                <CalendarButton />
-              </div>
-            </div>          
-        {this.state.isShowMap &&       <Map location={this.props.location} />}        
+      <div className="additional-info">
+        <div className="first-row">
+          <SocialButtons
+            link={`https://www.eventsfree.by/event/${id}`}
+            title={title}
+            isShowCount={false}
+            image={image}
+          />
+          {contacts && Object.keys(contacts).length > 0 && <Contacts contacts={contacts} />}
+          {location && (
+            <div>
+              <Location location={location} />
+              <button className="btn-link" onClick={this.handleClick}>
+                {!this.state.isShowMap ? 'Показать на карте' : 'Скрыть'}{' '}
+              </button>
+            </div>
+          )}
+          <div>
+            <CalendarButton />
+          </div>
+        </div>
+        {this.state.isShowMap && <Map location={this.props.location} />}
       </div>
     );
   }
