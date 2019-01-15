f5a19651cf0fcb516b76043510743ef34dc98558
VladKopylov <vladkopylov3@gmail.com>
2019-01-15 16:55:58 +0200

    fix https

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -1,3 +1,3 @@
 export const API = '/api';
-// export const API = 'http://www.eventsfree.by/api';
-export const domain = 'http://www.eventsfree.by';
+// export const API = 'https://www.eventsfree.by/api';
+export const domain = 'https://www.eventsfree.by';
