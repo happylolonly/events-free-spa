5320041a180ba747bfd965e1a4cc22f8a33bcc42
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-01-27 10:30:48 +0700

    remove nav link to events

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -9,14 +9,16 @@ const Header = () => {
   return (
     <div className="header">
       <div className="title">
-        <span className="title__main">Events Free {isChristmasHolidays() && <Santa />}</span>
+        <Link to="/">
+          <span className="title__main">Events Free {isChristmasHolidays() && <Santa />}</span>
+        </Link>
         <span className="title__sub">Все бесплатные мероприятия в одном месте</span>
       </div>
       <nav className="nav">
         <ul className="nav__list">
-          <li className="nav__item">
+          {/* <li className="nav__item">
             <Link to="/events">Мероприятия</Link>
-          </li>
+          </li> */}
           {/* <li className="nav__item"><Link to='/weekevents'>На неделю</Link></li> */}
           <li className="nav__item">
             <Link to="/about">О приложении</Link>
